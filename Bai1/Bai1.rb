a8b2e205e7511d340c4e12e19ed8a1f15cee0b5e
luongvinhlong9 <luongvinhlong35@gmail.com>
2022-08-29 21:18:52 +0700

    commit tuan 1

---
+++ Bai1/Bai1.rb
@@ -0,0 +1,17 @@
+print "Nhap ho ten cua ban"
+name = gets
+print "Nhap MSV cua ban"
+sid = gets
+print "Xin chao "+name + sid
+print "Nhap so bat ki tu ban phim"
+number = gets
+print "So vua nhap: "+number
+print "Nhap 2 so de so sanh"
+number1 = gets
+number2 = gets
+if(number1>number2)
+  print "So lon la : "+number1
+elseif(number1<number2)
+  print "So lon la: "+number2
+else
+  print "2 so bang nhau"
